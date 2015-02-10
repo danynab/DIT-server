1b268463a86636a718ed4685cd7c35cdc66ad6d4
Dani Meana <danielmeanallorian@gmail.com>
2015-02-10 02:41:56 +0100

    Se muestran los eventos cercanos con lat, lng y radio. Codigo sucio. Pendiente de limpiar. AVISO: instalar dependencia.

--- application/services/event_service.py
+++ application/services/event_service.py
@@ -3,6 +3,7 @@ __author__ = 'Dani'
 from application.model import Event
 from application import db
 from sqlalchemy import desc
+from haversine import haversine
 
 
 class EventService:
@@ -15,7 +16,30 @@ class EventService:
         events = Event.query
         if category_id is not None:
             events = events.filter(Event.category_id == category_id)
-        return EventService._partition_events(events, from_id, elements)
+
+        if from_id is not None:
+            event = Event.query.get(from_id)
+            if event is not None:
+                events_temp = events.order_by(desc(Event.time)).filter(Event.time >= event.time)
+                row = 0
+                for event_temp in events_temp:
+                    row += 1
+                    if event_temp.id == event.id:
+                        break
+            else:
+                return []
+        else:
+            row = 0
+        events = events.order_by(desc(Event.time))
+
+        def is_closed(event_lat, event_lng):
+            if lat is None or lng is None or radius is None:
+                return True
+            else:
+                return haversine((float(lat), float(lng)), (float(event_lat), float(event_lng))) <= float(radius)
+
+        events = [event for event in events[int(row):int(row) + int(elements)] if is_closed(event.lat, event.lng)]
+        return events
 
     @staticmethod
     def find_events_by_user_id(user_id, from_id, elements):
@@ -33,6 +57,7 @@ class EventService:
 
     @staticmethod
     def _partition_events(events, from_id, elements):
+
         if from_id is not None:
             event = Event.query.get(from_id)
             if event is not None:
