366aa832607fdbf10acb6098556ad42319825e3a
Marco Vidal García <marcovidal67@gmail.com>
2015-02-11 00:11:27 +0100

    SOAP API change

--- application/wsservices/category_wsservice.py
+++ application/wsservices/category_wsservice.py
@@ -7,6 +7,6 @@ from application.model import Category
 class CategoryWSService:
     @staticmethod
     def get_all_categories():
-        categories_from_ws = client.service.get_all_category()[0]
+        categories_from_ws = client.service.get_all_categories()[0]
         categories_from_ws = sorted(categories_from_ws, key=lambda category: category.name)
         return [Category(category.id, category.name, colors.next_color()) for category in categories_from_ws]
